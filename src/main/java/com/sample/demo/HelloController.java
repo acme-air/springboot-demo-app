d61f4effa5da4b297425ac2708a66a1b7daceb9c
Hariharan Venkitachalam <vharihar@in.ibm.com>
2020-10-05 05:29:36 +0530

    fixed

--- src/main/java/com/sample/demo/HelloController.java
+++ src/main/java/com/sample/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/", method = GET)
+    @RequestMapping("/", method = "GET")
     public String index() {
         return "Hello there! Greetings from Spring Boot!";
     }
